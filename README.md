d61db7b4269ef78dbddfe486b620a7e68b348389
Alexander Korolev <alexander.korolev.germany@gmail.com>
2021-02-28 12:02:00 +0100

    webpack example

---
+++ README.md
@@ -0,0 +1,9 @@
+# actix-web-static-files Examples
+
+## Legal
+
+Dual-licensed under `MIT` or the [UNLICENSE](http://unlicense.org/).
+
+## Features
+
+A collection of [actix-web-static-files](https://github.com/kilork/actix-web-static-files) examples.
---
+++ webpack/build.rs
@@ -0,0 +1,14 @@
+use actix_web_static_files::NpmBuild;
+
+fn main() {
+    NpmBuild::new("web")
+        .install()
+        .unwrap()
+        .run("build")
+        .unwrap()
+        .target("web/dist/bundle")
+        .change_detection()
+        .to_resource_dir()
+        .build()
+        .unwrap();
+}

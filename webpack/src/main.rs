4f35d5e47843fd6934eebbb3f0d6b9a91b3787b4
Alexander Korolev <alexander.korolev.germany@gmail.com>
2025-03-28 21:25:25 +0100

    tests webpack

--- webpack/src/main.rs
+++ webpack/src/main.rs
@@ -4,11 +4,18 @@ include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || {
+    let listen = std::env::var("LISTEN").unwrap_or_else(|_| "127.0.0.1:8084".into());
+    let server = HttpServer::new(|| {
         let generated = generate();
         App::new().service(actix_web_static_files::ResourceFiles::new("/", generated))
     })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    .bind(listen)?;
+
+    if let Some(addr) = server.addrs().first() {
+        println!("{:05}", addr.port());
+    }
+
+    let handle = actix_web::rt::spawn(server.run());
+
+    handle.await?
 }

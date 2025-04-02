475e0ed782a4a11a5b38b8ba2d50524e54595745
Alexander Korolev <alexander.korolev.germany@gmail.com>
2025-04-02 22:42:33 +0200

    use OUT_DIR for webpack output dir

--- webpack/web/webpack.config.js
+++ webpack/web/webpack.config.js
@@ -1,17 +1,19 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'actix-web-static-files WebPack',
+      title: "actix-web-static-files WebPack",
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist', 'bundle'),
+    filename: "main.js",
+    path: process.env.OUT_DIR
+      ? path.resolve(process.env.OUT_DIR, "web", "dist", "bundle")
+      : path.resolve(__dirname, "dist", "bundle"),
   },
 };
